0d09b354d7ba5369c8590f033c7dbbb8c0631fa4
Bojana-Draganovska <draganovska.bojana@gmail.com>
2023-04-24 17:45:57 +0200

    Done with one column

--- script.js
+++ script.js
@@ -3,29 +3,68 @@ const engine = new BABYLON.Engine(canvas, true);
 
 // Setting up scene in BabylonJS
 function createScene() {
+  const scene = new BABYLON.Scene(engine);
 
-const scene = new BABYLON.Scene(engine); 
+  const camera = new BABYLON.FreeCamera(
+    "camera",
+    new BABYLON.Vector3(0, 1, -5),
+    scene
+  );
+  camera.attachControl();
+  camera.speed = 0.25;
 
-const camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0,1,-5), scene);
-camera.attachControl();
+  const light = new BABYLON.HemisphericLight(
+    "light",
+    new BABYLON.Vector3(1, 1, 0),
+    scene
+  );
 
-const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0), scene);
+  light.intensity = 1;
 
-light.intensity = 0.5;
-
-
-return scene;
-
+  return scene;
 }
 
 const scene = createScene();
 
 engine.runRenderLoop(() => {
-    scene.render();
+  scene.render();
 });
 
-const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 10, height: 10}, scene);
+const ground = BABYLON.MeshBuilder.CreateGround(
+  "ground",
+  { width: 100, height: 100 },
+  scene
+);
 
-const ladybug = BABYLON.MeshBuilder.CreateSphere("ladybug", {diameter: 1}, scene);
+const column = BABYLON.MeshBuilder.CreateCylinder(
+  "column",
+  { height: 2, diameter: 0.5, tessellation: 24 },
+  scene
+);
 
-ladybug.position = new BABYLON.Vector3(0,1,0);
+column.position = new BABYLON.Vector3(0, 1, 0);
+
+// Setting Materials
+
+const createColumnMaterial = () => {
+  const columnMaterial = new BABYLON.StandardMaterial("columnMaterial", scene);
+  const diffuseTex = new BABYLON.Texture("assets/textures/wood/wood-diffuse.jpg",scene); //color  map
+
+  columnMaterial.diffuseTexture = diffuseTex;
+
+  const normalTex = new BABYLON.Texture("assets/textures/wood/wood-normal.jpg",scene); //normal map
+
+  columnMaterial.bumpTexture = normalTex;
+
+  const aoTex = new BABYLON.Texture("assets/textures/wood/wood-ao.jpg", scene);
+
+  columnMaterial.ambientTexture = aoTex;
+
+  const specTex = new BABYLON.Texture("assets/textures/wood/wood-spec.jpg", scene);
+
+  columnMaterial.specularTexture = specTex;
+
+  return columnMaterial;
+};
+
+column.material = createColumnMaterial();
