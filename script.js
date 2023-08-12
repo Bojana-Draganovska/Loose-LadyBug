4db39aaedcff674e59945446486d07be899e5860
Bojana-Draganovska <draganovska.bojana@gmail.com>
2023-08-12 11:20:13 +0200

    heading and added place for the score

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const playBtn = document.getElementById("startBtn");
+// const playBtn = document.getElementById("startBtn");
 
-playBtn.addEventListener('click', function() {
-  playBtn.classList.add('hidden');
-})
+// playBtn.addEventListener('click', function() {
+//   playBtn.classList.add('hidden');
+// })
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext('2d');
@@ -11,8 +11,10 @@ const ctx = canvas.getContext('2d');
 var background = new Image();
 background.src = "https://user-images.githubusercontent.com/18351809/46888871-624a3900-ce7f-11e8-808e-99fd90c8a3f4.png";
 
+
   background.onload = function(){
-    ctx.drawImage(background,0,0);   
+    ctx.canvas.width = background.width; ctx.canvas.height = background.height; 
+    ctx.drawImage(background, 0, 0, background.width, background.height);   
   } 
 
 
